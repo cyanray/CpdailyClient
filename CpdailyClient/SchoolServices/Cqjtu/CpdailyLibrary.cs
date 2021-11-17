1a4890455958428fcfbe7edb93c1134eb5185aad
cyanray <a484599279@hotmail.com>
2021-11-17 23:46:00 +0800

    完善 CpdailyLibrary 服务.

--- CpdailyClient/SchoolServices/Cqjtu/CpdailyLibrary.cs
+++ CpdailyClient/SchoolServices/Cqjtu/CpdailyLibrary.cs
@@ -14,6 +14,42 @@ namespace Cpdaily.SchoolServices.Cqjtu
     {
         const string UserAgent = "Mozilla/5.0 (Linux; Android 11; Redmi K30 5G Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36  cpdaily/8.2.17 wisedu/8.2.17";
 
+        public async Task<string> LoginAsync(string cookie)
+        {
+            string? url = "http://libopp.cqjtu.edu.cn/";
+            string host = url;
+            var cookieContainer = new CookieContainer();
+            MatchCollection matchCollection = Regex.Matches(cookie, @"([^=]+)=([^;]+);?\s*");
+            foreach (Match item in matchCollection)
+            {
+                if (item.Groups.Count != 3) continue;
+                cookieContainer.Add(new Uri(url), new Cookie(item.Groups[1].Value, item.Groups[2].Value));
+                cookieContainer.Add(new Uri("http://ids.cqjtu.edu.cn/"), new Cookie(item.Groups[1].Value, item.Groups[2].Value));
+            }
+            do
+            {
+                if (!url.StartsWith("http"))
+                {
+                    url = $"{host}{url}";
+                }
+                RestClient client = new RestClient(url)
+                {
+                    CookieContainer = cookieContainer,
+                    FollowRedirects = false,
+                    UserAgent = UserAgent
+                };
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Upgrade-Insecure-Requests", "1");
+                request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                request.AddHeader("X-Requested-With", "com.wisedu.cpdaily");
+                var response = await client.ExecuteGetAsync(request);
+                url = response.Headers.Where(x => x.Name == "Location").Select(x => x.Value.ToString()).FirstOrDefault();
+            } while (!string.IsNullOrEmpty(url));
+            string result = string.Join(" ", cookieContainer.GetAllCookies().Select(x => $"{x.Name}={x.Value};"));
+            return result;
+        }
+
+
         public async Task ReserveAsync(string cookie, string libraryName, DateTime date)
         {
             string url = $"http://libopp.cqjtu.edu.cn/tsg.asp?riqi={date:yyyy-MM-dd}&quyu={libraryName}&vehicle=同意";
--- Examples/CqjtuLibrary/Program.cs
+++ Examples/CqjtuLibrary/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cpdaily;
+using Newtonsoft.Json;
+using Cqjtu = Cpdaily.SchoolServices.Cqjtu;
+
+namespace MobileLogin
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("使用方法：dotnet CqjtuLibrary.dll \"Cookie\"");
+                return;
+            }
+            string cookie = args[0];
+            Console.WriteLine($"Cookie: {cookie}");
+            try
+            {
+                var library = new Cqjtu.CpdailyLibrary();
+                // 图书馆 App 需要额外登录一次
+                cookie = await library.LoginAsync(cookie);
+                await library.ReserveAsync(cookie, "南岸馆", DateTime.Parse("2021-11-18"));
+                Console.WriteLine("已经尝试预约!");
+                var logs = await library.GetReservationsAsync(cookie);
+                foreach (var item in logs)
+                {
+                    Console.WriteLine($"{item.Id}. {item.LibraryName} {item.Date:D}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
